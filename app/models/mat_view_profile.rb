32a8427c905ef3bc8f9fcaf07929f04f6f0f56e4
Salvador Fuentes Jr <fuentesjr@gmail.com>
2019-04-19 20:43:40 -0700

    Make skills attribute from profile view available as a collection for simpler processing on the front-end

--- app/models/mat_view_profile.rb
+++ app/models/mat_view_profile.rb
@@ -1,7 +1,18 @@
 class MatViewProfile < ApplicationRecord
-  self.primary_key = :my_unique_identifier_field
 
   def self.refresh
     Scenic.database.refresh_materialized_view(:mat_view_profiles, concurrently: true, cascade: false)
   end
+
+  def as_json(options={})
+    super.tap do |hash|
+      hash['skills'] = skills_as_coll if hash['skills']
+    end
+  end
+
+  private
+    def skills_as_coll
+      self.skills.split(", ").map {|s| {name: s} }
+    end
+
 end
